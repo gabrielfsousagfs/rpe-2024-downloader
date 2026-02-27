5562acaa4c409d9f0b8812eb5f2d52c0c75f9db0
gabrielfsousagfs <gabrielf.sous97@gmail.com>
2026-02-27 17:03:22 +0000

    Voltando ao que funcionou da primeira vez

--- download.py
+++ download.py
@@ -3,31 +3,23 @@ import os
 import zipfile
 from playwright.async_api import async_playwright
 
-# =========================
-# CONFIGURAÇÃO DO BLOCO
-# =========================
-START_ID = 0
-END_ID = 999  # ajuste conforme quiser expandir
+START_ID = 0000
+END_ID = 0499  # teste curto
 SAVE_FOLDER = "pdfs_rpe_2024"
-ZIP_NAME = f"RPE_2024_{START_ID}_{END_ID}.zip"
+ZIP_NAME = "RPE_2024_0000_0499.zip"
 
 BASE_URL = "https://sistema-registropublicodeemissoesapi.fgv.br/GenerateReport/GenerateInventoryReport/{}/18/true"
 
 os.makedirs(SAVE_FOLDER, exist_ok=True)
 
 async def main():
-    print("Script iniciou.")
     download_count = 0
 
-    print("Iniciando Playwright...")
-
     async with async_playwright() as p:
-        print("Playwright iniciado.")
-        print("Abrindo Chromium...")
         browser = await p.chromium.launch(headless=True)
-        print("Chromium aberto.")
         context = await browser.new_context(accept_downloads=True)
         page = await context.new_page()
+        print("Iniciando loop for")
 
         for participant_id in range(START_ID, END_ID + 1):
             formatted_id = f"{participant_id:04d}"
@@ -46,23 +38,15 @@ async def main():
                 download_count += 1
                 print(f"✔ PDF salvo {formatted_id}")
 
-            except Exception:
-                print(f"✖ Não encontrado {formatted_id}")
-
-            # pausa para evitar bloqueio
-            await asyncio.sleep(0.8)
+            except Exception as e:
+                print(f"✖ Falhou {formatted_id}")
 
         await browser.close()
 
-    # Criar ZIP apenas se houver PDFs
     if download_count > 0:
         with zipfile.ZipFile(ZIP_NAME, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for file in os.listdir(SAVE_FOLDER):
                 zipf.write(os.path.join(SAVE_FOLDER, file), file)
-
-        print(f"ZIP criado: {ZIP_NAME}")
-    else:
-        print("Nenhum PDF encontrado neste bloco.")
 
     print("===================================")
     print(f"Total de PDFs baixados: {download_count}")
